e4e235480388dc4915d8d1f5445958c5b1102245
chawn <30426016+nealchawn@users.noreply.github.com>
2018-06-25 00:06:17 -0500

    Added content to contact form and quote page

--- PriceQuote.php
+++ PriceQuote.php
@@ -4,8 +4,29 @@
 </head>
 
 <body>
-<h1>Get Organized</h1>
+<h3>Get A Quote</h3>
 
+<form action="/action_page.php" method="get">
+<label for="Size">How Many Documents Do You Have?</label><br>
+  <input type="checkbox" name="vehicle" value="Bike"> Small(1000 or less)<br>
+  <input type="checkbox" name="vehicle" value="Car" checked="checked"> Medium(1000 to 10,000)<br>
+    <input type="checkbox" name="vehicle" value="Car" checked="checked"> Large(10,000+)<br>
+	
+<label for="Size">What Services Would You Like?</label><br>
+  <input type="checkbox" name="vehicle" value="Bike">We Will Organize Your Data<br>
+  <input type="checkbox" name="vehicle" value="Bike">Digitally Backup Your Files Safely and Securly<br>
+  <input type="checkbox" name="vehicle" value="Bike">Online or Local Storage<br>
+  <input type="checkbox" name="vehicle" value="Bike">Optional Webiste to Access Data (Online or local Option)<br>
+  <input type="checkbox" name="vehicle" value="Bike">Export to Excel, CSV, PDF or Print<br>
+  <input type="checkbox" name="vehicle" value="Bike">Documentation and Training for Non Computer People<br>
+  <input type="checkbox" name="vehicle" value="Bike">Customer Service<br>
+  <input type="checkbox" name="vehicle" value="Bike">Go Into Any Medium<br>
+  <input type="checkbox" name="vehicle" value="Bike">Imports From Excel<br>
+  <input type="checkbox" name="vehicle" value="Bike">User Interface<br>
+  <input type="checkbox" name="vehicle" value="Bike">Easy Access To Organized Data<br>
+  <input type="checkbox" name="vehicle" value="Bike">Typed Vs Scanned<br>
+  <input type="submit" value="Submit"><br>
+</form>
 
 </body>
 
