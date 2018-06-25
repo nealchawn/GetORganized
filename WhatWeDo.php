80db79427a623094daa34f46666a2c46343a029a
chawn <30426016+nealchawn@users.noreply.github.com>
2018-06-24 23:35:08 -0500

    First Commit Build this Business

---
+++ WhatWeDo.php
@@ -0,0 +1,33 @@
+<html>
+<head>
+<?php include 'header.php'; ?>
+</head>
+
+<body>
+
+  <div class="forum"><p style="font-family:Times New Roman, Georgia, Serif;font-size: 100%;">
+  <h1>What We DO</h2>
+  <p>
+  We Will help your business by Taking all of your records, data, and Paper Documents and putting it in a Database. 
+  But We also offer A range of other services.
+  </p>
+  <ul>
+  <li>We Will Organize Your Data</li>
+  <li>Digitally Backup Your Files Safely and Securly</li>
+  <li>Online or Local Storage</li>
+  <li>Optional Webiste to Access Data (Online or local Option)</li>
+  <li>Export to EXcel, CSV, PDF or Print</li>
+  <li>Documentation and Training for Non Computer People</li>
+  <li>Customer Service</li>
+  <li>Go Into Any Medium</li>
+  <li>Imports From Excel</li>
+  <li>User Interface</li>
+  <li>Easy Access To Organized Data</li>
+  <li>Typed Vs Scanned</li>
+  </ul>
+  </p>
+  </div>
+
+</body>
+
+</html>
---
+++ index.php
@@ -0,0 +1,21 @@
+<html>
+<head>
+<?php include 'header.php'; ?>
+</head>
+
+<body>
+<h1>Get Organized</h1>
+    <div class="position-relative overflow-hidden p-3 p-md-5 m-md-3 text-center bg-light" id="bodytext">
+      <div class="col-md-5 p-lg-5 mx-auto my-5">
+        <h1 class="display-4 font-weight-normal">Get Organized and Get Control Over Your Business</h1>
+        <p class="lead font-weight-normal">We will help you Take Care Of all of those Files Around Your Office.</p>
+        <a class="btn btn-outline-secondary" href="#">Coming soon</a>
+      </div>
+      <div class="product-device box-shadow d-none d-md-block"></div>
+      <div class="product-device product-device-2 box-shadow d-none d-md-block"></div>
+    </div>
+
+
+</body>
+
+</html>
